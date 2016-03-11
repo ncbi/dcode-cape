94ed901296b69f104f7621033508b2a4750987d9
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2016-03-11 13:30:56 -0500

    Fixing bug on parsing line

--- src/libkm/src/SNPFactory.cpp
+++ src/libkm/src/SNPFactory.cpp
@@ -290,6 +290,7 @@ int SNPFactory::ProcessSNPFromFile(char* snpFileName, unsigned long int neighbor
         } 
         str = buffer;
         while ((newLine = strchr(str, '\n')) != NULL) {
+            *newLine = 0;
             if (*backupLine != 0) {
                 if (strlen(backupLine) + strlen(str) + 1 > backupLineSize) {
                     backupLineSize += backupLineSize;
