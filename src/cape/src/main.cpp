e1c0fe1872b2619d492df333cd4728d538cd6830
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2016-05-31 13:44:26 -0400

    Updating STD containers

--- src/cape/src/main.cpp
+++ src/cape/src/main.cpp
@@ -14,6 +14,7 @@
 #include <vector>
 #include <locale>
 #include <unordered_map>
+#include <unordered_set>
 #include <map>
 #include <set>
 #include <vector>
--- src/kweight/src/main.cpp
+++ src/kweight/src/main.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <map>
 #include <set>
 
